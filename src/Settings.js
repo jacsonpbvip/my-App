4717c96472a3ea48deddc57b5cc809ab5d87a866
jacsonpbvip <116603652+jacsonpbvip@users.noreply.github.com>
2023-05-06 16:08:52 -0300

    Primeiro Commit

---
+++ src/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Settings() {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Here are some settings.</p>
+    </div>
+  );
+}
+
+export default Settings;
---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD-iv3FRXdBLDEZSxzP5DV2wdRkd03xdcs",
+    authDomain: "firesilicon-20849.firebaseapp.com",
+    projectId: "firesilicon-20849",
+    storageBucket: "firesilicon-20849.appspot.com",
+    messagingSenderId: "978357416067",
+    appId: "1:978357416067:web:27fc90fe4a6b0bad0fabb6",
+    measurementId: "G-W01RHYL7QV"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+
+async function getData() {
+    const usersRef = firestore.collection('values');
+    const usersSnapshot = await usersRef.get();
+    const users = [];
+    usersSnapshot.forEach((doc) => {
+        users.push({
+            id: doc.id,
+            ...doc.data(),
+        });
+    });
+    return users;
+}
+
+export { firestore, getData };
